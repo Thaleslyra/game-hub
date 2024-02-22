0ae1bb567c2b4238879f7627f369cae813abafd7
Thales <thalespipa@hotmail.com>
2024-02-22 12:49:39 -0300

    show loading skeletons

---
+++ src/components/GameCardSkeleton.tsx
@@ -0,0 +1,14 @@
+import { Card, CardBody, Skeleton, SkeletonText } from "@chakra-ui/react";
+
+const GameCardSkeleton = () => {
+  return (
+    <Card width={200} borderRadius={10} overflow={"hidden"}>
+      <Skeleton height={200}></Skeleton>
+      <CardBody>
+        <SkeletonText />
+      </CardBody>
+    </Card>
+  );
+};
+
+export default GameCardSkeleton;
